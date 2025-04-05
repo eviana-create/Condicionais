13864ca764b7469b782c05a4119f101620d71388
Emerson Viana <missonzito@outlook.com>
2025-04-05 15:42:01 -0300

    programa de temperatura

---
+++ main.py
@@ -0,0 +1,7 @@
+temperatura = int(input("Digite Temperatura: "))
+if temperatura >= 100:
+    aguaFerve = True
+    evaporacao = "Muito Rapido"
+    print("Agua ferve", aguaFerve, evaporacao)
+else:
+    print("Agua não esta quente")
