30540902661cdde415ff77d6c2f67a83ff66fced
Emerson Viana <missonzito@outlook.com>
2025-04-07 21:34:27 -0300

    definido se a variavel possui raizes reais

--- FormulaDeBhaskara.py
+++ FormulaDeBhaskara.py
@@ -5,8 +5,15 @@ c = int(input("Digite um valor para c: "))
 
 delta = b ** 2 - 4 * a * c
 
-raiz1 = (-b + math.sqrt(delta)) / (2 * a)
-raiz2 = (-b - math.sqrt(delta)) / (2 * a)
+if delta == 0:
+    raiz1 = (-b + math.sqrt(delta)) / (2 * a)
+    print("A unica raiz é: ", raiz1)
 
-print("A primeira raiz é: ",raiz1)
-print("A segunda raiz é: ",raiz2)
+else:
+    if delta < 0:
+        print("Esta equação não possui raizes reais")
+    else:
+        raiz1 = (-b + math.sqrt(delta)) / (2 * a)
+        raiz2 = (-b - math.sqrt(delta)) / (2 * a)
+        print("A primeira raiz é: ",raiz1)
+        print("A segunda raiz é: ",raiz2)
